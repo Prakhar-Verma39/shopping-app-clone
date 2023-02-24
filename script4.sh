12d7bf49bbae964c7ba924efae0780cb7e46bdb5
Prakhar Verma <prakharverma39.gmial.com>
2023-02-24 17:36:55 +0530

    eight commit from ubantu terminal.

--- script4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#read month name and year and print the calendar for same
- Month=0 
-i Year=0
-echo "Enter the month in words and year of a calendar :"
-read Month
-read Year
-cal "$Month" "$Year"
