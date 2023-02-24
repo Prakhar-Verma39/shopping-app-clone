c47e59060ac8c079aff92ea51ff03a7a8c55509f
Prakhar Verma <prakharverma39.gmial.com>
2023-02-24 19:51:14 +0530

    third time removing the files

--- script2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# a script to calculate simple interest
-PA=1 RoI=1 TiY=1
-echo "Enter the principal amount , rate of interest and time in year :"
-read PA
-read RoI
-read TiY
-echo "The value of simple interest is `echo "scale=4; ($PA * $RoI * $TiY) / 100 "|bc` "
--- script3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# a shell script to print date(dd-mm-yyyy), time(hh-mm-ss), the name of working shell and terminal 
-date +"Today's date is %d-%m-%Y and time is %H-%M-%S %p"
-echo "The name of working shell is $SHELL and the name of terminal is $TERM" 
